79d2acfbfbcc86f72b83e946ded8b71d23a2d1f3
GraffVishenka <65270494+GraffVishenka@users.noreply.github.com>
2024-03-25 08:26:07 +0500

    f

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,85 +1,48 @@
+import getCards from './get-cards.js'
 
-const earphonesTabCardWrapper = document.querySelector('.earphones').querySelector(".tab__card-wrapper");
-const sellersTabCardWrapper = document.querySelector('.sellers').querySelector(".tab__card-wrapper");
+export default function tabs(tabNames,dataCards,tabContainerElement ){
+    // tabNames - string[]
+    // dataCards - {
+    //     cardTitle: string,
+    //     reviews: number,
+    //     price: number,
+    // }[][]
+    // tabContainerElement - Element | null
 
-const sellersTabBtnWrapper = document.querySelector('.sellers').querySelector(".tab__btn-wrapper");
+    if(!tabContainerElement) {
+        return
+    }
+    
+    const tabCardWrapper =  document.querySelector(`${tabContainerElement}`).querySelector('.tab__card-wrapper')
+    const tabBtnWrapper =document.querySelector(`${tabContainerElement}`).querySelector('.tab__btn-wrapper')
 
+    tabBtnWrapper.addEventListener("click",(event)=>{
+        const target = event.target
+        const emptyWrapper = document.createElement('div')
+        emptyWrapper.classList.add('tab__card-wrapper')
+        
+    
+        const ovalButtons = tabBtnWrapper.querySelectorAll('button')
+        ovalButtons.forEach(btn =>{
+            btn.classList.remove('active')
+        })
+        target.classList.add('active')
 
+        tabNames.forEach((tab,index) =>{
+          if( tab == target.innerText){
+              getCards(dataCards[index],tabCardWrapper)
 
-const cardInfoTopPicksList = [{
-  cardTitle: "Беcпроводные наушники №1",
-  reviews:100,
-  price:32,
-},{
-  cardTitle: "Беcпроводные наушники №2",
-  reviews:130,
-  price:95,
-},{
-  cardTitle: "Беcпроводные наушники №3",
-  reviews:88,
-  price:123,
-}]
-const cardInfoWatchesList = [{
-  cardTitle: "Беcпроводные наушники №456",
-  reviews:100,
-  price:32,
-},{
-  cardTitle: "Беcпроводные наушники №981",
-  reviews:130,
-  price:95,
-},{
-  cardTitle: "Беcпроводные наушники №330",
-  reviews:88,
-  price:123,
-}]
-
-function getCards(cardInfoList, elem) {
-  const arr = cardInfoList.forEach(cardInfo => {
-    const tab = `<div class="tab__card card">
-        <div class="card__product">
-          <img
-            class="card__img"
-            src="/src/assets/card__headphons.png"
-            alt="card__headphons"
-          />
-          <div class="card__descr">
-            <h4 class="card__text">${cardInfo.cardTitle}</h4>
-            <div class="card__rating">
-              <img src="/src/assets/red__star.svg" alt="rating" />
-              <img src="/src/assets/red__star.svg" alt="rating" />
-              <img src="/src/assets/red__star.svg" alt="rating" />
-              <img src="/src/assets/red__star.svg" alt="rating" />
-              <img src="/src/assets/red__star.svg" alt="rating" />
-              <p class="card__rating-count">${cardInfo.reviews} Reviews</p>
-            </div>
-            <p class="card__price">$${cardInfo.price}</p>
-          </div>
-        </div>
-        <button class="card__btn button-card">Add to cart</button>
-        <div class="card__discount">
-          <p>Save 60%</p>
-        </div>
-        </div>`;
-    elem.insertAdjacentHTML("beforeend", tab);
-  });
+          }
+        })        
+    })
 }
-sellersTabBtnWrapper.addEventListener("click",(event)=>{
-  const target = event.target
-  const emptyWrapper = document.createElement('div')
-  emptyWrapper.classList.add('tab__card-wrapper')
-  
 
-  const ovalButtons = sellersTabBtnWrapper.querySelectorAll('button')
-  ovalButtons.forEach(btn =>{
-      btn.classList.remove('active')
 
-  })
-  target.classList.add('active')
-  if(target.innerText == "Top Picks"){  
-      getCards(cardInfoTopPicksList,sellersTabCardWrapper)
-  }else{ 
-      getCards(cardInfoWatchesList,sellersTabCardWrapper,false)
-  }
-  
-})
-getCards(cardInfoTopPicksList,sellersTabCardWrapper)
+
+
+
+
+
+
+
+ 
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = (env) => {
+module.exports = (env)=>{
     return {
         mode:env.mode || 'development',
-        entry:path.resolve(__dirname,'src','index.js'),
+        entry:path.resolve(__dirname,'src','main.js'),
         output: {
             path: path.resolve(__dirname,'build'),
             filename: '[name].[contenthash].js',
-            clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({template:path.resolve(__dirname,'public','index.html')
-        })
-    ]
+            clean:true,
+        },
+        plugins:[
+            new HtmlWebpackPlugin({template:path.resolve(__dirname,'public','index.html')})
+        ],
+        module: {
+            rules: [
+              {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              },
+            ],
+        },
+        devServer: {           
+            port: env.port || 3000,
+            open:true
+        },
+        
     }
 }
